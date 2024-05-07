743f80a13c872e6a850915a68da62d7492f7c422
NCSUAL <ncsual@naver.com>
2024-05-07 23:14:08 +0900

    study
    1

---
+++ security_study/src/main/java/project/security_study/controller/TestController.java
@@ -0,0 +1,15 @@
+package project.security_study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @GetMapping("/test")
+    public String test(){
+        return "테스트 완료";
+    }
+}
