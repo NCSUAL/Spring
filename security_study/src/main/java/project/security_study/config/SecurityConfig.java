0e9637dfa9a8523a01f9450e680c50d976e04785
NCSUAL <ncsual@naver.com>
2024-05-08 00:19:33 +0900

    study
    1

---
+++ security_study/src/main/java/project/security_study/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package project.security_study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf((auth)-> auth.disable()); //csrf 공격을 방어한다
+        http.authorizeRequests((auth) -> auth
+
+                        // /** -> 모든 경로
+                        .requestMatchers("/user/**").authenticated() // /user/**로 들어온 사람들은 인증이 필요하다
+                        .requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // /manager/**로 들어온 사람들은 권한이 필요함
+                        .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
