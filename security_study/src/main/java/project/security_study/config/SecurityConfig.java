ec8e834bf6eb97a4a3fcaab34175d50f2d6c2cef
NCSUAL <ncsual@naver.com>
2024-05-10 17:24:37 +0900

    security
    공부 - 2일차

--- security_study/src/main/java/project/security_study/config/SecurityConfig.java
+++ security_study/src/main/java/project/security_study/config/SecurityConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf((auth)-> auth.disable()); //csrf 공격을 방어한다
@@ -16,10 +22,14 @@ public class SecurityConfig {
 
                         // /** -> 모든 경로
                         .requestMatchers("/user/**").authenticated() // /user/**로 들어온 사람들은 인증이 필요하다
-                        .requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // /manager/**로 들어온 사람들은 권한이 필요함
+                        .requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                        // /manager/**로 들어온 사람들은 권한이 필요함
                         .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
                         .anyRequest().permitAll()
                 );
+
+        //로그인 페이지 설정
+        http.formLogin((auth)-> auth.loginPage("/login")); //이거 get요청임 ㅋㅋ
 
         return http.build();
     }
--- security_study/src/main/java/project/security_study/controller/TestController.java
+++ security_study/src/main/java/project/security_study/controller/TestController.java
@@ -1,11 +1,24 @@
 package project.security_study.controller;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import project.security_study.Repository.StudyRepository;
+import project.security_study.model.StudyData;
+import project.security_study.service.StudyService;
 
 @Controller
 public class TestController {
+
+    private final StudyService studyService;
+    public TestController(StudyService studyService){
+        this.studyService = studyService;
+    }
 
     @GetMapping("/test")
     public String test(){
@@ -29,4 +42,22 @@ public class TestController {
     public String admin(){
         return "admin";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/joinForm")
+    public String join(){
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinForm(StudyData studyData){
+        studyService.login(studyData);
+        return "redirect:/login";
+    }
+
+
 }
